fbbf5ee580718147f289b1ba43674f85747a7fc5
NikiRufolo <nikirufolo@gmail.com>
2022-12-13 19:57:55 +0100

    4

--- snack-3/index.php
+++ snack-3/index.php
@@ -3,19 +3,63 @@ Creare un array di array. Ogni array figlio avrà come chiave una data in questo
 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
 Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z -->
 
-$ArrayDate = [
-    ]
+<?php
 
+$posts = [
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+foreach($posts as $date => $array) {
+echo "<br><b># 
+$date</b><br>";
+foreach ($array as $key => $value) {
+echo "<li>$value[title]</li>" . "<li>$value[author]</li>" . "<li>$value[text]</li>";
+}
+}
+?>
+
+<!-- //all'array post associa il primo giro di ciclo e alla prima dimensione nella posizione dove si trovano le date associamo alla variabile $date
+// entra "=>" e dove ti trovi ora per richiamarti uso la variabile $array
+foreach ($posts as $date => $array) {
+
+    //stampa le date in maiuscolo -->
+    <!-- echo "<br><b># $date</b><br>"; -->
+    <!-- //cicla sull'array all'interno di date e associa alla variabile $key le chiavi dell'array.
+    // => entra nella chiave e associa il valore di ogni chiave a $value.
+    for -->
--- snack-4/index.php
+++ snack-4/index.php
@@ -1,3 +1,15 @@
-Snack 4
-<!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà
-contenere lo stesso numero più di una volta -->
+<!-- Snack 4 -->
+<!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+<?php
+$array = [];
+for( $i =0; $i < 15; $i++ ) {
+    $number = rand(1, 1000);
+    if(!in_array($number, $array)){
+        $array[] = $number;
+    }
+};
+foreach($array as $value){
+    echo "<div>Number is: $value</div>";
+}
+
+?>
